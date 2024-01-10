5744f81b54e2c8e47be6b5aebf026fb61cde537b
Sakib <sakibmohammad7679@gmail.com>
2024-01-11 00:52:29 +0600

    find longest word problem solve

--- 10.findLongestWord.js
+++ 10.findLongestWord.js
@@ -1,1 +1,11 @@
 //Problem 10: Find the Longest Word
+
+const longestWord = (sentence) => {
+  const words = sentence.split(" ");
+  let maxLength = 0;
+  for (let word of words) {
+    maxLength = Math.max(maxLength, word.length);
+  }
+  return maxLength;
+};
+console.log(longestWord("My name is sakibbb"));
